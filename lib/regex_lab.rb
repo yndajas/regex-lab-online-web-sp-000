3bd7c9e856eac2d252479966a11a93599b0538b4
Ynda Jas <yndajas@gmail.com>
2020-07-02 14:48:04 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  word.scan(/\bun\w*ing\b/)
+  text.scan(/\bun\w*ing\b/)
 end
 
 def words_five_letters_long(text)
