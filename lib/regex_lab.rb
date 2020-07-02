1304b607267541750a92cce9cc0843b2049740e5
Ynda Jas <yndajas@gmail.com>
2020-07-02 14:57:10 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,11 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.scan(/[A-Z].*\W/)
+  if text.match(/[A-Z].*\W/)
+    true
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)
